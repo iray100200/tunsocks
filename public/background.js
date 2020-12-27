b228c89e8657ed223e3b2009182109071f634bc1
iray100200 <iray100200@hotmail.com>
2020-12-27 22:13:33 +0800

    1

--- public/background.js
+++ public/background.js
@@ -1,5 +1,28 @@
 /* global chrome */
 
-chrome.webRequest.onBeforeRequest.addListener((details) => {
-  console.log(details)
-}, {urls: ["<all_urls>"]})
+function setProxy () {
+  var config = {
+    mode: chrome.proxy.Mode.FIXED_SERVERS,
+    rules: {
+      singleProxy: {
+        scheme: chrome.proxy.Scheme.SOCKS5,
+        host: "localhost"
+      },
+      bypassList: ["baidu.com"]
+    }
+  }
+  
+  chrome.proxy.settings.set({
+    value: config, 
+    scope: 'regular'
+  },
+  function(config) {
+    console.log(config)
+  })
+
+  chrome.proxy.onProxyError.addListener((detail) => {
+    console.log(detail)
+  })
+}
+
+setProxy()
